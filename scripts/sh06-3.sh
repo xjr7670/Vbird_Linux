6ef84af1c4705a2786b6c84c177463878a8ecdd4
Cavin <xjr30226@126.com>
2017-03-12 14:06:44 +0800

    使用elif

---
+++ scripts/sh06-3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Program:
+#     This program shows the user's choice
+# History:
+# 2017/03/12    Cavin    First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+read -p "Please input (Y/N) : " yn
+if [ "$yn" == "Y" ] || [ "$yn" == "y" ]; then
+    echo "OK, continue"
+elif [ "$yn" == "N" ] || [ "$yn" == "n" ]; then
+    echo "Oh, interrupt!"
+else
+    echo "I don't know what your choice is" && exit 0
+fi
