062d76c1121cfdcef0bef3574cda4ca2bb7d75ca
Cavin <xjr30226@126.com>
2017-03-12 13:36:23 +0800

    从1加到100

---
+++ scripts/sh14.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Program:
+#     Use loop to calculate "1+2+3+...+100" result
+# History:
+# 2017/03/12    Cavin    First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+s=0
+i=0
+while [ "$i" != "100" ]
+do
+    i=$(($i+1))
+    s=$(($s+$i))
+done
+echo "The result of '1+2+3+...+100' is ==> $s"
