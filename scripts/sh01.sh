ccaab2f6b3145712a70f70c60d2196e265c891eb
Cavin <xjr30226@126.com>
2017-03-12 14:02:00 +0800

    输出hello world“

---
+++ scripts/sh01.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Program:
+#     This program show "Hello World!" in your screen.
+# History:
+# 2017/3/12 Cavin First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+echo -e "Hello World! \a \n"
+exit 0
