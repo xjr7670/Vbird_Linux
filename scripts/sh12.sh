2ba32ee78ab2113f239802eead9ab91fa4920e15
Cavin <xjr30226@126.com>
2017-03-12 14:09:03 +0800

    使用case..in并接收用户输入

---
+++ scripts/sh12.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Program:
+#     This script only accept the flowing parameter: one, two or three
+# History:
+# 2017/03/12    Cavin    First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+
+echo "This program will print your selection !"
+# read -p "Input your choice: " choice
+# case $choice in
+case $1 in
+    "one")
+        echo "Your choice is ONE"
+        ;;
+    "two")
+        echo "Your choice is TWO"
+        ;;
+    "three")
+        echo "Your choice is THREE"
+        ;;
+    *)
+        echo "Usage $0 {the|two|three}"
+        ;;
+esac
